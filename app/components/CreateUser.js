036a164319ba9c76f83da1ec2b9af111522cc7fd
Stephanie Andrews <seandrews11@gmail.com>
2017-02-01 15:36:39 -0700

    create user hooked up

--- app/components/CreateUser.js
+++ app/components/CreateUser.js
@@ -4,28 +4,46 @@ export default class CreateUser extends Component {
   constructor(){
     super();
     this.state = {
-      username: '',
+      name: '',
+      email: '',
       password: '',
     }
   }
 
+  handleCreateUser (name, email, password) {
+    const server = ('http://localhost:3000/api/users/new')
+    fetch(server, {
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({name: name, email: email, password: password})
+    })
+    .then(response => response.json())
+    .then(json => console.log(json));
+  }
+
   render(){
+
+    let {name, email, password} = this.state
+
     return(
       <section>
         <h2>Not a user? Make an account</h2>
         <input
           placeholder='Name'
-          value={this.state.username}
-          onChange={() => console.log('hi')} />
+          value={name}
+          onChange={(e) => this.setState({name: e.target.value})} />
         <input
           placeholder='Email'
-          value={this.state.username}
-          onChange={() => console.log('hi')} />
+          value={email}
+          onChange={(e) => this.setState({email: e.target.value})} />
         <input
           placeholder='Password'
-          value={this.state.password}
-          onChange={() => console.log('hi')} />
-        <button>Sign Up</button>
+          value={password}
+          onChange={(e) => this.setState({password: e.target.value})} />
+        <button onClick={()=> this.handleCreateUser(name, email, password) }>Sign Up</button>
       </section>
     )
   }

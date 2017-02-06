7b5493f826a7602298e92cd0a271b7b63e9a1589
Stephanie Andrews <seandrews11@gmail.com>
2017-02-06 03:53:13 -0700

    make stubs to get app test to load

--- app/components/App.test.js
+++ app/components/App.test.js
@@ -1,13 +1,27 @@
 import React from 'react';
-
+import { Provider } from 'react-redux'
 import { expect, assert } from 'chai';
 import { shallow, mount } from 'enzyme'
 
-import App from './App';
+import { defaultFakeStore } from '../components/testing-stubs/store-stub'
+import { fakeStore } from '../components/testing-stubs/store-stub'
+import defaultApiMovies from '../components/testing-stubs/default-movies-api-call-results'
 
-describe('App', function () {
-  it.skip('renders as a div', () => {
-    const wrapper = shallow(<App />)
+import App from './App'
+
+const path = require('path');
+
+
+App.prototype.componentWillMount = () => {
+  jest.mock(defaultApiMovies)
+}
+
+const activeUser = {id: 1, name: 'taylor',}
+
+describe('App', () => {
+  it('renders as a div', () => {
+    const wrapper = shallow(<App userSignInReducer={activeUser}/>)
     assert.equal(wrapper.type(), 'div')
   });
+
 });
